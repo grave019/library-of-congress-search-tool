bdbc67a8e1b6107126cabb9ce2fc2e0a6b986592
Brent Graves <graves.brent@gmail.com>
2022-04-12 11:08:09 -0400

    added initial function handleSearchFormSubmit.

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+    
+    var searchInputVal = document.querySelector('#search-input').value;
+    var formatInputVal = document.querySelector('#format-input').value;
+
+    if (!searchInputVal) {
+        console.error('You must have a search input value!')
+    }
+}
